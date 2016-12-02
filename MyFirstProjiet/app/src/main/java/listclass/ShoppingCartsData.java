8e7ddc61f1b9f05663a6ed6e3cc074d13025f5b0
lhx <1628472468@qq.com>
2016-12-02 17:35:50 +0800

    购物车的listview和逻辑

---
+++ MyFirstProjiet/app/src/main/java/listclass/ShoppingCartsData.java
@@ -0,0 +1,16 @@
+package listclass;
+
+/**
+ * Created by Administrator on 2016/12/2.
+ */
+public class ShoppingCartsData {
+    boolean check_Show_hidden =false;
+
+    public boolean getisCheck_Show_hidden() {
+        return check_Show_hidden;
+    }
+
+    public void setCheck_Show_hidden(boolean check_Show_hidden) {
+        this.check_Show_hidden = check_Show_hidden;
+    }
+}
